52f377217185250e8d26b2afb1285d8bfc1f525c
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-06-07 22:36:13 +0200

    add filters ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import { Container } from './components/Container/Container';
+import Filters from './components/Filters';
+import Jobs from './components/Jobs';
 
 function App() {
    return (
       <Container>
-         <h1>JoBoard 🛹</h1>
+         <Filters />
+         <Jobs />
       </Container>
    );
 }
---
+++ src/components/Jobs/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Jobs = () => {
+   return (
+      <div>
+         <div>
+            <form>
+               <div>
+                  <input type="text" />
+                  <button>O</button>
+               </div>
+               <input type="text" />O
+            </form>
+         </div>
+         <div>36 offers found</div>
+      </div>
+   );
+};
+
+export default Jobs;
